947795d59df36dbcbce063c864a2ad25d358dc1a
AlexanderCaceres <92133600+AlexanderCaceres@users.noreply.github.com>
2022-08-08 08:28:50 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
+probots.initColorSensor(probots.conexiones_ret(conn.CON3))
 basic.forever(function () {
-	
+    if (probots.getSensedColorValue() == probots.colors_ret(Names_colors.Red)) {
+        basic.showIcon(IconNames.Heart)
+    } else if (probots.getSensedColorValue() == probots.colors_ret(Names_colors.Green)) {
+        basic.showIcon(IconNames.SmallHeart)
+    } else if (probots.getSensedColorValue() == probots.colors_ret(Names_colors.Blue)) {
+        basic.showIcon(IconNames.Rollerskate)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
 })
